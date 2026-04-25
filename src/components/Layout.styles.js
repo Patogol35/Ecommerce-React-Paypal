324c016d4863197e0e4782660a64b51445981fcb
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-25 15:36:42 -0500

    Update Layout.styles.js

--- src/components/Layout.styles.js
+++ src/components/Layout.styles.js
@@ -3,18 +3,18 @@ import { alpha } from "@mui/material/styles";
 export const layoutStyles = (theme) => ({
   footer: {
   textAlign: "center",
-  paddingTop: 12,
-  paddingBottom: 12,
+  paddingTop: 16,
+  paddingBottom: 16,
   marginTop: "auto",
   color: theme.palette.text.secondary,
 
   borderTop: `1px solid ${
     theme.palette.mode === "dark"
-      ? alpha("#fff", 0.45)
-      : alpha("#000", 0.45)
+      ? alpha("#fff", 0.35)
+      : alpha("#000", 0.25)
   }`,
 
   backgroundColor: theme.palette.background.paper,
-  transition: "background-color 0.3s ease",
+  transition: "all 0.3s ease",
 },
 });
